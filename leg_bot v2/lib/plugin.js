d60eb386f9f7f958f8b9563eee8d3a51762ba1ec
Robert Johnston <anaerin@gmail.com>
2016-11-09 05:51:07 -0600

    Working working working...

---
+++ leg_bot v2/lib/plugin.js
@@ -0,0 +1,26 @@
+﻿var EventEmitter = require("events");
+
+class Plugin extends EventEmitter {
+	constructor(client) {
+		super();
+		this.client = client;
+	}
+
+	static get name() {
+		throw Error("Name is not implemented");
+		//return "Plugin name";
+	}
+
+	static get description() {
+		throw Error("Description is not implemented");
+		//return "Description goes here";
+	}
+
+	static get configuration() {
+		throw Error("Configuration is not implemented");
+		//return [];
+	}
+	
+}
+
+module.exports = Plugin;
---
+++ leg_bot v2/lib/plugins/barks.js
@@ -0,0 +1,32 @@
+﻿var Plugin = require("../plugin.js");
+
+class Barks extends Plugin {
+	constructor(client) {
+		super(client);
+	}
+	static get description() {
+		return "Allows simple responses to in-chat queries, or timed repeating of static messages";
+	}
+	static get name() {
+		return "Barks";
+	}
+	static get configuration() {
+		return [
+			{
+				name: "Active",
+				type: "Boolean",
+				default: true
+			}, 
+			{
+				name: "Barks",
+				type: "Callback",
+				callback: "EditBarks"
+			}
+		];
+	}
+	EditBarks() {
+
+	}
+}
+
+module.exports = Barks;
